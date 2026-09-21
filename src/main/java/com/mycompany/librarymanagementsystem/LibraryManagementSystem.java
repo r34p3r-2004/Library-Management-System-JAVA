079ca9e4a72a0ad9ea62f0ae7b59b2c8512df9ba
sesan <sesan@SESANDU>
2026-09-21 15:29:57 +0530

    Project Initialized

---
+++ src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.librarymanagementsystem;
+
+/**
+ *
+ * @author sesan
+ */
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
